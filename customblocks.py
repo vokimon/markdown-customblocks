86f215a8a3bb193821c198855cdb2513bb751f14
David García Garzón <voki@canvoki.net>
2020-06-12 11:48:46 +0200

    customblocks: just the blocktype

---
+++ customblocks.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+from markdown.extensions import Extension
+from markdown.blockprocessors import BlockProcessor
+from markdown.util import etree
+import re
+
+
+class CustomBlocksExtension(Extension):
+    """ Admonition extension for Python-Markdown. """
+
+    def extendMarkdown(self, md):
+        """ Add Admonition to Markdown instance. """
+        md.registerExtension(self)
+
+        md.parser.blockprocessors.register(CustomBlocksProcessor(md.parser), 'admonition', 105)
+
+
+class CustomBlocksProcessor(BlockProcessor):
+	RE = re.compile(r'(?:^|\n)::: *([\w\-]+)(?:\n|$)')
+
+	def test(self, parent, block):
+		return self.RE.search(block)
+
+	def run(self, parent, blocks):
+		block = blocks.pop(0)
+		match = self.RE.search(block)
+		mainClass = match.group(1)
+		div = etree.SubElement(parent, 'div')
+		div.set('class', '%s' % (mainClass))
+		return True
+
+"""
+# TODO
+- Takes content
+- Content is reprocessed
+- Subblocs are reprocessed
+- In the middle of a paragraph
+- Indentation over within the block
+- key parameters
+- parameters with commas
+"""
+
+
+
+
+def makeExtension(**kwargs):  # pragma: no cover
+    return CustomBlocksExtension(**kwargs)
---
+++ customblocks_test.py
@@ -0,0 +1,59 @@
+import unittest
+from markdown import markdown
+from markdown import test_tools
+
+class CustomBlockExtension_Test(test_tools.TestCase):
+
+	def setUp(self):
+		self.default_kwargs = dict(
+			extensions = [
+				'customblocks',
+			],
+		)
+
+	def assertMarkdown(self, markdown, html, **kwds):
+		self.assertMarkdownRenders(
+			self.dedent(markdown),
+			self.dedent(html),
+			**kwds)
+
+	def test_simple(self):
+		self.assertMarkdown("""\
+			::: myblock
+			""", """\
+			<div class="myblock"></div>
+			""")
+
+	def test_simple_noSpace(self):
+		self.assertMarkdown("""\
+			:::myblock
+			""", """\
+			<div class="myblock"></div>
+			""")
+
+	def test_simple_manySpaces(self):
+		self.assertMarkdown("""\
+			:::  myblock
+			""", """\
+			<div class="myblock"></div>
+			""")
+
+
+
+	def _test_innerAndOuter(self):
+
+		self.assertMarkdownRenders(self.dedent("""\
+			::: block
+				inner
+			outer
+			"""),self.dedent("""\
+			<div class='block'>
+			<p>inner</p>
+			</div>
+			<p>outer</p>
+			"""))
+
+
+	
+
+
