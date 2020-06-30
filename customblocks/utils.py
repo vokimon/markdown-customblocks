23ea0fa24e25f8e37199386b9e8025a8128b7dff
David García Garzón <voki@canvoki.net>
2020-06-30 20:04:47 +0200

    style: tabs removed

--- customblocks/utils.py
+++ customblocks/utils.py
@@ -2,41 +2,41 @@ from markdown.util import etree
 
 
 def E(tag, *children, **attribs):
-	tag, *classes = tag.split('.')
-	attributes = dict()
+    tag, *classes = tag.split('.')
+    attributes = dict()
 
-	def blend(adict):
-		if '_class' in adict:
-			aclass = adict.pop('_class')
-			if aclass:
-				classes.append(aclass)
-		attributes.update(adict)
+    def blend(adict):
+        if '_class' in adict:
+            aclass = adict.pop('_class')
+            if aclass:
+                classes.append(aclass)
+        attributes.update(adict)
 
-	for child in children:
-		if isinstance(child, dict):
-			blend(child)
-	blend(attribs)
+    for child in children:
+        if isinstance(child, dict):
+            blend(child)
+    blend(attribs)
 
-	element = etree.Element(tag or 'div',
-		{'class': ' '.join(classes)} if classes else {},
-		**{
-			k:format(v)
-			for k,v in attributes.items()
-			if v is not None
-		}
-	)
-	for child in children:
-		if isinstance(child, dict):
-			continue
-		if type(child) == str:
-			if len(element):
-				element[-1].tail = (element[-1].tail or '') + child
-			else:
-				element.text = (element.text or '') + child
-			continue
-		if type(child) == etree.Element:
-			element.append(child)
-	return element
+    element = etree.Element(tag or 'div',
+        {'class': ' '.join(classes)} if classes else {},
+        **{
+            k:format(v)
+            for k,v in attributes.items()
+            if v is not None
+        }
+    )
+    for child in children:
+        if isinstance(child, dict):
+            continue
+        if type(child) == str:
+            if len(element):
+                element[-1].tail = (element[-1].tail or '') + child
+            else:
+                element.text = (element.text or '') + child
+            continue
+        if type(child) == etree.Element:
+            element.append(child)
+    return element
 
 
 # vim: et ts=4 sw=4
--- customblocks/utils_test.py
+++ customblocks/utils_test.py
@@ -5,149 +5,149 @@ from textwrap import dedent
 
 class ETest(unittest.TestCase):
 
-	def assertXml(self, e, expected):
-		self.assertMultiLineEqual(
-			etree.tostring(e,'unicode'),
-			dedent(expected.strip()))
-		
+    def assertXml(self, e, expected):
+        self.assertMultiLineEqual(
+            etree.tostring(e,'unicode'),
+            dedent(expected.strip()))
+        
 
-	def test_tag_named(self):
-		e = E('div')
-		self.assertXml(e, """\
-			<div />
-		""")
+    def test_tag_named(self):
+        e = E('div')
+        self.assertXml(e, """\
+            <div />
+        """)
 
-	def test_tag_noName(self):
-		e = E('')
-		self.assertXml(e, """\
-			<div />
-		""")
+    def test_tag_noName(self):
+        e = E('')
+        self.assertXml(e, """\
+            <div />
+        """)
 
-	def test_tag_class(self):
-		e = E('.myclass')
-		self.assertXml(e, """\
-			<div class="myclass" />
-		""")
+    def test_tag_class(self):
+        e = E('.myclass')
+        self.assertXml(e, """\
+            <div class="myclass" />
+        """)
 
-	def test_tag_manyClasses(self):
-		e = E('.myclass.otherclass')
-		self.assertXml(e, """\
-			<div class="myclass otherclass" />
-		""")
+    def test_tag_manyClasses(self):
+        e = E('.myclass.otherclass')
+        self.assertXml(e, """\
+            <div class="myclass otherclass" />
+        """)
 
-	def test_attribute(self):
-		e = E('', attr1='value')
-		self.assertXml(e, """\
-			<div attr1="value" />
-		""")
+    def test_attribute(self):
+        e = E('', attr1='value')
+        self.assertXml(e, """\
+            <div attr1="value" />
+        """)
 
-	def test_attribute_integer(self):
-		e = E('', attr1=200)
-		self.assertXml(e, """\
-			<div attr1="200" />
-		""")
+    def test_attribute_integer(self):
+        e = E('', attr1=200)
+        self.assertXml(e, """\
+            <div attr1="200" />
+        """)
 
-	def test_attribute_attributeEncode(self):
-		e = E('', attr1="""a&<>"'z""")
-		self.assertXml(e, """\
-			<div attr1="a&amp;&lt;&gt;&quot;'z" />
-		""")
+    def test_attribute_attributeEncode(self):
+        e = E('', attr1="""a&<>"'z""")
+        self.assertXml(e, """\
+            <div attr1="a&amp;&lt;&gt;&quot;'z" />
+        """)
 
-	def test_child(self):
-		e = E('', E('child'))
-		self.assertXml(e, """\
-			<div><child /></div>
-		""")
+    def test_child(self):
+        e = E('', E('child'))
+        self.assertXml(e, """\
+            <div><child /></div>
+        """)
 
-	def test_text(self):
-		e = E('', "content")
-		self.assertXml(e, """\
-			<div>content</div>
-		""")
+    def test_text(self):
+        e = E('', "content")
+        self.assertXml(e, """\
+            <div>content</div>
+        """)
 
-	def test_text_afterChild(self):
-		e = E('', E('child'), "content")
-		self.assertXml(e, """\
-			<div><child />content</div>
-		""")
+    def test_text_afterChild(self):
+        e = E('', E('child'), "content")
+        self.assertXml(e, """\
+            <div><child />content</div>
+        """)
 
-	def test_text_twice(self):
-		e = E('', "first", "later")
-		self.assertXml(e, """\
-			<div>firstlater</div>
-		""")
+    def test_text_twice(self):
+        e = E('', "first", "later")
+        self.assertXml(e, """\
+            <div>firstlater</div>
+        """)
 
-	def test_text_twice_afterChild(self):
-		e = E('', E('child'), "first", "later")
-		self.assertXml(e, """\
-			<div><child />firstlater</div>
-		""")
+    def test_text_twice_afterChild(self):
+        e = E('', E('child'), "first", "later")
+        self.assertXml(e, """\
+            <div><child />firstlater</div>
+        """)
 
-	def test_text_afterSecondChild(self):
-		e = E('', E('child'), E('sibbling'), "content")
-		self.assertXml(e, """\
-			<div><child /><sibbling />content</div>
-		""")
+    def test_text_afterSecondChild(self):
+        e = E('', E('child'), E('sibbling'), "content")
+        self.assertXml(e, """\
+            <div><child /><sibbling />content</div>
+        """)
 
-	def test_text_interChildren(self):
-		e = E('', E('child'), "content", E('sibbling'))
-		self.assertXml(e, """\
-			<div><child />content<sibbling /></div>
-		""")
+    def test_text_interChildren(self):
+        e = E('', E('child'), "content", E('sibbling'))
+        self.assertXml(e, """\
+            <div><child />content<sibbling /></div>
+        """)
 
-	def test_attrib_asChildren(self):
-		e = E('', dict(attrib="value"))
-		self.assertXml(e, """\
-			<div attrib="value" />
-		""")
+    def test_attrib_asChildren(self):
+        e = E('', dict(attrib="value"))
+        self.assertXml(e, """\
+            <div attrib="value" />
+        """)
 
-	def test_attrib_keywordWins(self):
-		e = E('', dict(attrib="value1"), attrib="value2")
-		self.assertXml(e, """\
-			<div attrib="value2" />
-		""")
+    def test_attrib_keywordWins(self):
+        e = E('', dict(attrib="value1"), attrib="value2")
+        self.assertXml(e, """\
+            <div attrib="value2" />
+        """)
 
-	def test_attrib_settwice_laterWins(self):
-		e = E('', dict(attrib="value1"), dict(attrib="value2"))
-		self.assertXml(e, """\
-			<div attrib="value2" />
-		""")
+    def test_attrib_settwice_laterWins(self):
+        e = E('', dict(attrib="value1"), dict(attrib="value2"))
+        self.assertXml(e, """\
+            <div attrib="value2" />
+        """)
 
-	def test_attrib_none_unsets(self):
-		e = E('', attrib=None)
-		self.assertXml(e, """\
-			<div />
-		""")
+    def test_attrib_none_unsets(self):
+        e = E('', attrib=None)
+        self.assertXml(e, """\
+            <div />
+        """)
 
-	def test_classkey_specialUnderline(self):
-		e = E('', _class='myclass')
-		self.assertXml(e, """\
-			<div class="myclass" />
-		""")
+    def test_classkey_specialUnderline(self):
+        e = E('', _class='myclass')
+        self.assertXml(e, """\
+            <div class="myclass" />
+        """)
 
-	def test_classkey_additive(self):
-		e = E('.myclass', _class='other')
-		self.assertXml(e, """\
-			<div class="myclass other" />
-		""")
+    def test_classkey_additive(self):
+        e = E('.myclass', _class='other')
+        self.assertXml(e, """\
+            <div class="myclass other" />
+        """)
 
-	def test_classchild(self):
-		e = E('', dict(_class='myclass'))
-		self.assertXml(e, """\
-			<div class="myclass" />
-		""")
+    def test_classchild(self):
+        e = E('', dict(_class='myclass'))
+        self.assertXml(e, """\
+            <div class="myclass" />
+        """)
 
-	def test_classchild_additive(self):
-		e = E('.aclass', dict(_class='myclass'))
-		self.assertXml(e, """\
-			<div class="aclass myclass" />
-		""")
+    def test_classchild_additive(self):
+        e = E('.aclass', dict(_class='myclass'))
+        self.assertXml(e, """\
+            <div class="aclass myclass" />
+        """)
 
-	def test_classNone(self):
-		e = E('.aclass', dict(_class=None))
-		self.assertXml(e, """\
-			<div class="aclass" />
-		""")
+    def test_classNone(self):
+        e = E('.aclass', dict(_class=None))
+        self.assertXml(e, """\
+            <div class="aclass" />
+        """)
 
 
 
