b2b8b49207f959e9589f063c6ec54c12fffcbaa9
David García Garzón <voki@canvoki.net>
2020-06-30 18:59:19 +0200

    split return

--- customblocks/utils.py
+++ customblocks/utils.py
@@ -3,10 +3,11 @@ from markdown.util import etree
 
 def E(tag, **attribs):
 	tag, *classes = tag.split('.')
-	return etree.Element(tag or 'div',
+	element = etree.Element(tag or 'div',
 		{'class': ' '.join(classes)} if classes else {},
 		**{k:format(v) for k,v in attribs.items()}
 	)
+	return element
 
 
 # vim: et ts=4 sw=4
