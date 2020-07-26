84d658b3fc4ca9c06a40f485227b2b5dfe6c6efd
David García Garzón <voki@canvoki.net>
2020-07-26 17:03:43 +0200

    taking title from actual html

--- customblocks/utils_htmlmeta.py
+++ customblocks/utils_htmlmeta.py
@@ -1,8 +1,11 @@
 from yamlns import namespace as ns
+from bs4 import BeautifulSoup
 
 def extractInfo(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    titleElement = soup.find('title')
     return ns(
-        title = "My title",
+        title = titleElement.text,
     )
 
 
