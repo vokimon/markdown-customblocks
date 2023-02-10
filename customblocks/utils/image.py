ab3a786cd072f0cb61fb60f8130ef3f3e9d83507
David García Garzón <david.garcia@somenergia.coop>
2023-02-10 08:34:00 +0100

    new image.local() and evolved image.thumb()

--- customblocks/utils/image.py
+++ customblocks/utils/image.py
@@ -1,7 +1,10 @@
 from pathlib import Path
+from urllib.parse import urlparse
 import magic
 import base64
-from PIL import Image
+import mimetypes
+from PIL import Image, UnidentifiedImageError
+from .fetcher import Fetcher
 
 def embed(localfile):
     mime = magic.from_file(localfile, mime=True)
@@ -9,13 +12,38 @@ def embed(localfile):
         encoded = base64.b64encode(f.read()).decode('ascii')
         return f"data:{mime};base64,{encoded}"
 
-def thumbnail(localfile, max_width=200, max_height=200):
+def thumbnail(localfile, max_width=200, max_height=200, target=Path()):
     localfile = Path(localfile)
-    im = Image.open(localfile)
-    thumbnailfile = localfile.with_stem(localfile.stem+'.thumb')
+    try:
+        im = Image.open(localfile)
+    except UnidentifiedImageError as e:
+        return localfile
+    target.mkdir(parents=True, exist_ok=True)
+    thumbnailfile = target / localfile.with_stem(
+        localfile.stem+f'.thumb-{max_width}x{max_height}'
+    )
     im.thumbnail((max_width, max_height))
     im.save(thumbnailfile)
     return thumbnailfile
+
+def local(url, target:Path=Path()):
+    parsedurl = urlparse(url)
+    if not parsedurl.netloc:
+        return Path(url) # Local file
+    response = Fetcher('testcache').get(url)
+    remotepath = Path(parsedurl.path)
+    mime = response.headers.get('Content-Type', None)
+    extension = None
+    if mime:
+        extension = mimetypes.guess_extension(mime)
+    if not extension:
+        extension = remotepath.suffix
+    if not extension: # TODO: Untested
+        extension = '.dat'
+    localfile = target / (remotepath.stem+extension)
+    localfile.parent.mkdir(parents=True, exist_ok=True)
+    localfile.write_bytes(response.content)
+    return localfile
 
 
 # vim: et ts=4 sw=4
--- customblocks/utils/image_test.py
+++ customblocks/utils/image_test.py
@@ -1,9 +1,9 @@
 from ..testutils import sandbox_dir
-from .image import embed, thumbnail
+from .image import embed, thumbnail, local
 from markdown import test_tools
 from PIL import Image, ImageDraw
 from pathlib import Path
-
+import magic
 
 class Image_Test(test_tools.TestCase):
 
@@ -22,7 +22,7 @@ class Image_Test(test_tools.TestCase):
 
     def assertImageSize(self, imagefile, width, height):
         with Image.open(imagefile) as im:
-            self.assertEqual(im.size, (200, 113))
+            self.assertEqual(im.size, (width, height))
 
     def test_thumbnail(self):
         with sandbox_dir() as sandbox:
@@ -31,8 +31,64 @@ class Image_Test(test_tools.TestCase):
 
             thumb = thumbnail(imagefile)
 
-            self.assertEqual(str(thumb), 'myimage.thumb.jpg')
+            self.assertEqual(thumb, Path('myimage.thumb-200x200.jpg'))
             self.assertImageSize(thumb, width=200, height=113)
 
+    def test_thumbnail__with_target(self):
+        with sandbox_dir() as sandbox:
+            imagefile = 'myimage.jpg'
+            self.sample_image(imagefile)
+            target=Path('thumbnails')
+
+            thumb = thumbnail(imagefile, target=target)
+
+            self.assertEqual(thumb, target / Path('myimage.thumb-200x200.jpg'))
+            self.assertImageSize(thumb, width=200, height=113)
+
+    def test_thumbnail__with_svg_idempotent(self):
+        with sandbox_dir() as sandbox:
+            svgfile = Path("drawing.svg")
+            svgfile.write_text("<svg />")
+            self.assertEqual(thumbnail(svgfile), svgfile)
+
+    remote_webp_sample='https://www.gstatic.com/webp/gallery/5.webp'
+    remote_jpeg_sample='https://www.gstatic.com/webp/gallery/5.jpg'
+    remote_svg_sample='https://getsamplefiles.com/download/svg/sample-1.svg'
+
+    def test_local(self):
+        with sandbox_dir() as sandbox:
+            localfile = local(self.remote_jpeg_sample)
+            self.assertEqual(localfile, Path('5.jpg'))
+
+    def test_local__usupported_mime__takes_remote_extension(self):
+        with sandbox_dir() as sandbox:
+            localfile = local(self.remote_webp_sample)
+            self.assertEqual(localfile, Path('5.webp'))
+
+    def test_local__svg(self):
+        with sandbox_dir() as sandbox:
+            localfile = local(self.remote_svg_sample)
+            self.assertEqual(localfile, Path('sample-1.svg'))
+            self.assertEqual(magic.from_file(localfile, mime=True), 'image/svg+xml')
+
+    def test_local__target_directory(self):
+        with sandbox_dir() as sandbox:
+            outputdir = sandbox/'outputdir'
+            localfile = local(self.remote_webp_sample, target=outputdir)
+            self.assertEqual(localfile, outputdir / '5.webp')
+
+    def test_local__saves_content(self):
+        with sandbox_dir() as sandbox:
+            localfile = local(self.remote_jpeg_sample)
+            self.assertEqual(magic.from_file(localfile, mime=True), 'image/jpeg')
+            self.assertImageSize(localfile, width=1024, height=752)
+        
+    def test_local__with_local_idempotent(self):
+        with sandbox_dir() as sandbox:
+            already_local = 'this/is/a/file'
+            localfile = local(already_local)
+            self.assertEqual(localfile, Path(already_local))
+
+    
 
 # vim: et ts=4 sw=4
